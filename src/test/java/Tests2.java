a112ece025c2602280197494037b0644bae4fbd3
Alexey Drachuk <adrachuk@lohika.com>
2016-11-25 13:20:40 +0200

    user story #1069

---
+++ src/test/java/Tests2.java
@@ -0,0 +1,15 @@
+ /**
+ * Created by adrachuk on 22.11.2016.
+ */
+
+ import org.junit.Ignore;
+ import org.junit.Test;
+
+ import static org.junit.Assert.assertEquals;
+
+ public class Tests2 {
+     @Test
+     public void testPassed(){
+         assertEquals(true,true);
+     }
+ }
