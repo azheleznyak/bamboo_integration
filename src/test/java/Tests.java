3283a9073c60da8cc63201bd84b637d0ed56c0cc
Alexey Drachuk <adrachuk@lohika.com>
2016-11-28 18:25:57 +0200

    defect #1027

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -3,6 +3,8 @@
  */
 import org.junit.Ignore;
 import org.junit.Test;
+
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertEquals;
 public class Tests {
     @Test
@@ -12,7 +14,7 @@ public class Tests {
 
     @Test
     public void testPassed2(){
-        assertEquals(true,true);
+        assertEquals(true,false);
     }
 
     @Test
